b1fa4a69df194dda3a965cfaf48c952707012db7
Raphael Kusuma <77198909+RaphaLK@users.noreply.github.com>
2023-07-23 21:25:40 +0700

    uploaded a working bottom tab nav

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Tabs from './navigation/tabs';
+
+const App = () => {
+  return(
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
